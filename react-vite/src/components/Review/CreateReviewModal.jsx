1a2296fa07e6f639d7023da725e96ba0b3dc98ec
Muhammad Zubair <mzubair4193@gmail.com>
2024-02-06 21:17:01 -0800

    createReviewModal comp added

--- react-vite/src/components/Review/CreateReviewModal.jsx
+++ react-vite/src/components/Review/CreateReviewModal.jsx
@@ -8,6 +8,48 @@ import { thunkPostReview } from "../../redux/review";
 import { thunkLoadProductReviews } from "../../redux/review";
 
 const CreateReviewModal = () => {
-    ewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwhhhhhhhhhhhhhhhhhhhhhhhhhh666666666666666666666666666666666666666666666666666666666666666666666666666677777777777777jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+    const dispatch = useDispatch()
+    const [starRating, setStarRating] = useState(0)
+    const [review, setReview] = useState("")
+    const [errors, setErrors] = useState([])
+    const { closeModal } = useModal()
+
+    const { id } = useParams()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const intRating = parseFloat(starRating)
+
+        await dispatch(thunkPostReview(id, review, intRating))
+        await dispatch(thunkLoadProductReviews(id))
+        closeModal()
+    }
+
+    return (
+        <div className='createrevmodal'>
+            <h1 className='car'>Create a Review</h1>
+            <form onSubmit={handleSubmit} className="reviewform">
+                <input
+                    type="text"
+                    value={review}
+                    onChange={(e) => setStarRating(e.target.value)}
+                    required
+                    placeholder="Star Rating 1-5"
+                    className="starinput"
+                    min={1}
+                    max={5}
+                />
+                <textarea
+                    value={review}
+                    onChange={(e) => setReview(e.target.value)}
+                    placeholder="Share your thoughts on this product"
+                    className='reviewpost'
+                    maxLength={255}
+                />
+                <button type='submit' disabled={1 < starRating > 5 || review.length === 0} className='reviewsubmit'>Post Review</button>
+            </form>
+        </div>
+    )
+}
 
 export default CreateReviewModal
