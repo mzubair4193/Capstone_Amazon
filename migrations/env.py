6ac487c9a26b600652ab2107d85c357f1470933f
Muhammad Zubair <mzubair4193@gmail.com>
2024-02-11 15:48:21 -0800

    backend migrations fix

--- migrations/env.py
+++ migrations/env.py
@@ -4,8 +4,13 @@ import logging
 from logging.config import fileConfig
 
 from flask import current_app
-
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 from alembic import context
+
+import os
+environment = os.getenv('FLASK_ENV')
+SCHEMA = os.environ.get('SCHEMA')
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -85,7 +90,11 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    connectable = get_engine()
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
@@ -94,8 +103,13 @@ def run_migrations_online():
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
+        if environment == "production":
+            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
+        # Set search path to your schema (only in production)
         with context.begin_transaction():
+            if environment == "production":
+                context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
 
 
