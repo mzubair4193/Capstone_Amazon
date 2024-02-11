2445f53fe857444c2388637b1535ba39caafbfe4
Muhammad Zubair <mzubair4193@gmail.com>
2024-02-11 12:51:36 -0800

    category troubles

--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, NumberRange, Length
 class ProductForm(FlaskForm):
     name = StringField("Product Name", validators=[DataRequired(), Length(max=50)])
     description = TextAreaField("Description", validators=[DataRequired(), Length(max=1000)])
-    category = SelectField(choices=[('headphones', 'Headphones'), ('laptops', 'Laptops'), ('tv/video', 'TV/Video'), ('pet supplies', 'Pet Supplies'), ('kids toys', 'Kids Toys'), ('automotive/industrial', 'Automotive/Industrial'), ('sports/outdoors','Sports/Outdoors'), ('beauty/health', 'Beauty/Health'), ('movies/music/games', 'Movies/Music/Games')])
+    # category = SelectField(choices=[('headphones', 'Headphones'), ('laptops', 'Laptops'), ('tv/video', 'TV/Video'), ('pet supplies', 'Pet Supplies'), ('kids toys', 'Kids Toys'), ('automotive/industrial', 'Automotive/Industrial'), ('sports/outdoors','Sports/Outdoors'), ('beauty/health', 'Beauty/Health'), ('movies/music/games', 'Movies/Music/Games')])
     image = FileField("Image",validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
     price = DecimalField("Price", validators=[DataRequired(), NumberRange(min=0)], places=2)
     return_policy = StringField("Return Policy")
