a231f408d785d8d5cd4f3ce2743da117140c46b1
Muhammad Zubair <mzubair4193@gmail.com>
2024-02-11 16:30:50 -0800

    backend/frontend minor fixes

--- app/app/forms/review_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField,TextAreaField, SubmitField
-from wtforms.validators import DataRequired
-
-class ReviewForm(FlaskForm):
-    reviewText = TextAreaField('Review', validators=[DataRequired()])
-    starRating = IntegerField('Star Rating', validators=[DataRequired()])
-    submit = SubmitField('Submit')
