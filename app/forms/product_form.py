9a3f4b16f2577a5181ff9b0b2b9c242dfda0d459
Muhammad Zubair <mzubair4193@gmail.com>
2024-02-11 17:10:15 -0800

    debugging image err

--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -9,7 +9,7 @@ class ProductForm(FlaskForm):
     description = TextAreaField("Description", validators=[DataRequired(), Length(max=1000)])
     # category = SelectField(choices=[('headphones', 'Headphones'), ('laptops', 'Laptops'), ('tv/video', 'TV/Video'), ('pet supplies', 'Pet Supplies'), ('kids toys', 'Kids Toys'), ('automotive/industrial', 'Automotive/Industrial'), ('sports/outdoors','Sports/Outdoors'), ('beauty/health', 'Beauty/Health'), ('movies/music/games', 'Movies/Music/Games')])
     image = FileField("Image",validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
-    price = DecimalField("Price", validators=[DataRequired(), NumberRange(min=0)], places=2)
+    price = DecimalField("Price", validators=[DataRequired()])
     return_policy = StringField("Return Policy")
 
 
